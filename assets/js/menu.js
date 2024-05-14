1917400f74da4a5563be99826ad8af2f379516f6
ludovic lacroix <ludovic.lacroix82@gmail.com>
2024-05-14 08:58:24 +0200

    style/html

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,13 @@
 const btnAddCountry = document.querySelector('.add')
 const addCountry = document.querySelector('.addCountry')
+const addClose = document.querySelector('.country-header')
+const btnClose = addClose.querySelector('#close')
+
+console.log(btnClose);
 
 btnAddCountry.addEventListener('click',event=>{
+    addCountry.classList.toggle('open')
+})
+btnClose.addEventListener('click',event=>{
     addCountry.classList.toggle('open')
 })
