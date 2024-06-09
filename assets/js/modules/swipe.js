56c68266b83e8c8dac27551a0039c407d356086c
ludovic lacroix <ludovic.lacroix82@gmail.com>
2024-06-09 09:31:34 +0200

    responsive css + js

--- assets/js/modules/swipe.js
+++ assets/js/modules/swipe.js
@@ -2,74 +2,72 @@
  * Swipe : gère le défilement horizontal 
  * change les données des temparature en fonction du swip et de la ville visible
  * change les données du graphique des temperature en fonction du swipe
- * écouteur d'événements  touchsend
+ * écouteur d'événements touchend et click
  * @param {*} data donnée de l'api openweathermap
  */
 export function swipe(data) {
-    const todayHeader = document.querySelector('.today-header')
-    const todayHeaderOffesWidth = todayHeader.offsetWidth
-    let nbrCitySlected = 0
+    const todayHeader = document.querySelector('.today-header');
+    const todayHeaderOffesWidth = todayHeader.offsetWidth;
+    let nbrCitySlected = 0;
 
     const dateCurrent = new Date();
-    todayHeader.addEventListener('touchend', event => {
 
-        const citySelect = document.querySelectorAll('.country-select')
-        const dayTempList = document.querySelector('.day-temp')
+    const handleSwipe = () => {
+        const citySelect = document.querySelectorAll('.country-select');
+        const dayTempList = document.querySelector('.day-temp');
+        const dayItem = document.querySelector('.day-item');
 
-        const dayItem = document.querySelector('.day-item')
-
-       /**
-        * permet de changer le scrollLeft de l'elment pour affiché les autre ville en swipant 
-        * recupere l'ordre de chauqe element de temperature des ville
-        * @variable  nbrCitySlected est incrémenté qui permettra de swip la prochien ville
-        * & permet d'afficher les données pour les temperatures prochaine & pour les graphique 
-        *  */ 
+        /**
+         * permet de changer le scrollLeft de l'elment pour affiché les autre ville en swipant 
+         * recupere l'ordre de chauqe element de temperature des ville
+         * @variable  nbrCitySlected est incrémenté qui permettra de swip la prochien ville
+         * & permet d'afficher les données pour les temperatures prochaine & pour les graphique 
+         *  */ 
         if (nbrCitySlected < citySelect.length - 1) {
+            todayHeader.scrollLeft += todayHeaderOffesWidth - 20;
+            nbrCitySlected += 1;
+            const citySlectOpen = dayTempList.querySelector('#temp' + citySelect[nbrCitySlected].id);
+            citySlectOpen.classList.add('open');
 
-            todayHeader.scrollLeft += todayHeaderOffesWidth - 20
-            nbrCitySlected += 1
-            const citySlectOpen = dayTempList.querySelector('#temp' + citySelect[nbrCitySlected].id)
-            citySlectOpen.classList.add('open')
-
-            const canvasSlect = dayItem.querySelector('#myChart-' + citySelect[nbrCitySlected].id)
-            canvasSlect.classList.add('open-canvas')
+            const canvasSlect = dayItem.querySelector('#myChart-' + citySelect[nbrCitySlected].id);
+            canvasSlect.classList.add('open-canvas');
 
             // boucle qui permet de retirer la class open et open-canvas des elements non séléctionnés
             for (const cityNoSelect of citySelect) {
                 if (cityNoSelect.id != citySelect[nbrCitySlected].id) {
-                    const citySlectNoOpen = dayTempList.querySelector('#temp' + cityNoSelect.id)
-                    citySlectNoOpen.classList.remove('open')
+                    const citySlectNoOpen = dayTempList.querySelector('#temp' + cityNoSelect.id);
+                    citySlectNoOpen.classList.remove('open');
 
-                    const canvasNoSlect = dayItem.querySelector('#myChart-' + cityNoSelect.id)
-
-                    canvasNoSlect.classList.remove('open-canvas')
+                    const canvasNoSlect = dayItem.querySelector('#myChart-' + cityNoSelect.id);
+                    canvasNoSlect.classList.remove('open-canvas');
                 }
             }
         /**
          * Une fois que le swip arrive a la derière ville , celui-ci revient au 1er
          */
         } else {
-            todayHeader.scrollLeft = 0
-            nbrCitySlected = 0
+            todayHeader.scrollLeft = 0;
+            nbrCitySlected = 0;
 
-            const citySlectOpen = dayTempList.querySelector('#temp' + citySelect[nbrCitySlected].id)
-            citySlectOpen.classList.add('open')
+            const citySlectOpen = dayTempList.querySelector('#temp' + citySelect[nbrCitySlected].id);
+            citySlectOpen.classList.add('open');
 
-            const canvasSlect = dayItem.querySelector('#myChart-' + citySelect[nbrCitySlected].id)
-            canvasSlect.classList.add('open-canvas')
+            const canvasSlect = dayItem.querySelector('#myChart-' + citySelect[nbrCitySlected].id);
+            canvasSlect.classList.add('open-canvas');
             
             // boucle qui permet de retirer la class open et open-canvas des elements non séléctionnés
             for (const cityNoSelect of citySelect) {
                 if (cityNoSelect.id != citySelect[nbrCitySlected].id) {
-                    const citySlectNoOpen = dayTempList.querySelector('#temp' + cityNoSelect.id)
-                    citySlectNoOpen.classList.remove('open')
+                    const citySlectNoOpen = dayTempList.querySelector('#temp' + cityNoSelect.id);
+                    citySlectNoOpen.classList.remove('open');
 
-                    const canvasNoSlect = dayItem.querySelector('#myChart-' + cityNoSelect.id)
-                    canvasNoSlect.classList.remove('open-canvas')
+                    const canvasNoSlect = dayItem.querySelector('#myChart-' + cityNoSelect.id);
+                    canvasNoSlect.classList.remove('open-canvas');
                 }
             }
-
         }
+    };
 
-    })
+    todayHeader.addEventListener('touchend', handleSwipe);
+    todayHeader.addEventListener('click', handleSwipe);
 }
